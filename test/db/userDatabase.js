781e8fdbb8408571f9dbe02a906419c33618c88f
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-19 17:06:40 +0900

    s One-to-One (1:1) Mapping

--- test/db/userDatabase.js
+++ test/db/userDatabase.js
@@ -65,7 +65,6 @@ const readUsers = async () => {
         const data = users
         const msg = users.length !== 0 ? "Successfully read Users" : "Successfully read Users but empty"
 
-
         // return res.status(200).json({ isSuccess: true, msg, data })
         console.log({ isSuccess: true, msg, data })
     } catch (error) {
