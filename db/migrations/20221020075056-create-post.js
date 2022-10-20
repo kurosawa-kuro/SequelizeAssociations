66fb24a2396917749001a1ed6d7837a10d8fac41
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-20 17:48:54 +0900

    c

--- db/migrations/20221020075056-create-post.js
+++ db/migrations/20221020075056-create-post.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Posts', {
+    await queryInterface.createTable('posts', {
       id: {
         allowNull: false,
         autoIncrement: true,
